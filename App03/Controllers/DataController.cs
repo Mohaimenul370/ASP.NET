3a3b5cfa1cdfd471043935609df781ec39149eeb
Mohaimenul <mahadihasan1603@gmail.com>
2024-12-18 23:27:31 +0600

    Class 03

---
+++ App03/Controllers/DataController.cs
@@ -0,0 +1,50 @@
+﻿using App03.Models;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging.Signing;
+using System.IO;
+
+namespace App03.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MyForm(string usertext, int num)
+        {
+            if (string.IsNullOrEmpty(usertext)) { usertext = "No data is available"; }
+            if(num<=0) { num = 1; }
+
+            ViewBag.Usertext = usertext;
+            ViewBag.Num = num;
+
+            return View(); 
+        }
+        public IActionResult FileData()
+        {
+            return View();
+        }
+        public IActionResult ShowFileData()
+        {
+            string rootfolder = @"C:\";
+            string[] filelist = System.IO.Directory.GetFiles(rootfolder);
+
+            List<FileDetail> fdlist=new List<FileDetail>(); 
+            for (int i = 0; i < filelist.Length; i++) {
+
+                FileDetail d = new FileDetail()
+                {
+
+                    Id = i + 1,
+                    FileName = @System.IO.Path.GetFileNameWithoutExtension(filelist[i]),
+                    Extension = @System.IO.Path.GetExtension(filelist[i]),
+                    FullPath = @filelist[i]
+                };
+                fdlist.Add(d);
+            }
+            return View(fdlist);
+        }
+    }
+}
+ 
---
+++ App03/Models/FileDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace App03.Models
+{
+    public class FileDetail
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FullPath { get; set; }
+        public string Extension { get; set; }    
+        
+
+    }
+}
